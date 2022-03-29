ed57a074e64ee0cf34d4e0f94ed2225c2cedc17c
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-03-30 00:48:38 +0530

    Update NavBar.js

--- src/Header/NavBar.js
+++ src/Header/NavBar.js
@@ -1,10 +1,20 @@
 import './NavBar.css'
+import { useState } from 'react'
 export default function NavBar(){
-    return <>
-       <nav className="navbar">
-           <form>
-            <input type = 'text'/>
-           </form>
-       </nav>
+    const[myCar,setMyCar] = useState('')
+
+    const handleOnChange = (e)=>{
+        setMyCar(e.target.value)
+    }
+return<>
+    <form>
+        <label>Select option
+        <select value={myCar} onChange={handleOnChange} >
+            <option value='ford'>Ford</option>
+            <option value='bmw'>BMW</option>
+            <option value='audi'>Audi</option>
+        </select>
+        </label>
+    </form>
     </>
 }
