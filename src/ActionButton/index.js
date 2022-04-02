015d15c953d74a86eebfb319adce30230f5d4f05
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-04-02 17:33:22 +0530

    update

--- src/ActionButton/index.js
+++ src/ActionButton/index.js
@@ -1,9 +1,9 @@
 import './style.css'
 export default function ActionButton(props){
-    const handleOnClick = (e)=>{
-        alert(`you clicked ${props.btn}`)
+    const handleOnClick = ()=>{
+        alert(`you clicked button`)
     }
     return<>
-        <button onClick={handleOnClick} className="actionButton">{props.btn}</button>
+        <button onClick={handleOnClick} className="actionButton" ><img src= {props.btn} width = '25px' /></button>
     </>
 }
--- src/Header/NavBarIcons/index.js
+++ src/Header/NavBarIcons/index.js
@@ -1,12 +1,18 @@
 import ActionButton from "../../ActionButton";
-
 export default function NavBarIcons(){
+    const crossMark = require('../image/crossMark.png')
+    const smilingCat = require('../image/smilingCat.png')
+    const taxiImg = require('../image/taxiImg.png')
+    const burgerImg = require('../image/burgerImg.png')
+    const fruitsImg = require('../image/fruitsImg.jpg')
+    const heartsImg = require('../image/heartsImg.png')
     return<>
-        <ActionButton btn = 'X'/>
-        <ActionButton btn = 'A'/>
-        <ActionButton btn = 'B'/>
-        <ActionButton btn = 'C'/>
-        <ActionButton btn = 'D'/>
-        <ActionButton btn = 'E'/>
+    
+        <ActionButton btn = {crossMark}/>
+        <ActionButton btn = {smilingCat}/>
+        <ActionButton btn = {taxiImg}/>
+        <ActionButton btn = {burgerImg}/>
+        <ActionButton btn = {fruitsImg}/>
+        <ActionButton btn = {heartsImg}/>
     </>
 }
