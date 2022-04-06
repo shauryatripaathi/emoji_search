cd4a178a16d0745ef241d451c62c5dd2d5e21119
Shaurya <88775162+shauryatripaathi@users.noreply.github.com>
2022-04-07 00:18:10 +0530

    added css

--- src/Body/EmojiGrid/index.js
+++ src/Body/EmojiGrid/index.js
@@ -3,11 +3,12 @@ import data from '../../data/data.json'
 export default function EmojiGrid(props){
     return(
         <>
+        
             {data.filter((emoji => emoji.keywords.includes(props.searchText))).map((item,index)=>{
                 return (
-                    <li className='emojiList'>
+                    <div>
                         {item.symbol}
-                    </li>
+                    </div>
                 )
             })}
         
