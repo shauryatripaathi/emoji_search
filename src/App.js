9ce7ecb2972f4fb3129fbb0d60ba7083ff4cc11a
Shaurya <88775162+shauryatripaathi@users.noreply.github.com>
2022-04-06 23:48:18 +0530

    Update App.js
    removed display component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Display from './Body/Display';
 import HeaderSearch from './Header/HeaderSearch';
 
 function App() {
   return (
     <>
     <HeaderSearch/>
-    <Display/>
     </>
     )
 }
