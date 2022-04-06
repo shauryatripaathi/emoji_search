a5cff4c1734bb73de7f06b8e5fc8024080f984b1
Shaurya <88775162+shauryatripaathi@users.noreply.github.com>
2022-04-06 18:57:00 +0530

    unnecessary file Removed

--- src/Header/NavBar/index.js
+++
@@ -1,9 +0,0 @@
-import SearchBar from '../SearchBar'
-import './style.css'
-export default function NavBar(){
-    return<>
-        <nav className="navBar">
-            <SearchBar/>
-        </nav>
-    </>
-}
