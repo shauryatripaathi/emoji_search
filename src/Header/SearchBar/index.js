d65e228bf11b10beee92a4c144f53f4247e82f65
Shaurya <88775162+shauryatripaathi@users.noreply.github.com>
2022-04-07 13:11:11 +0530

    Update index.js

--- src/Header/SearchBar/index.js
+++ src/Header/SearchBar/index.js
@@ -1,16 +1,7 @@
- 
 export default function SearchBar(props) {
-
-    const handleOnSubmit = (event) => {
-        event.preventDefault()
-    }
-
     return <>
-        <form onSubmit={handleOnSubmit}>
             <input type='search'
                 value={props.value}
-                onChange={props.handleChange} />
-        </form>
-       
+                onChange={props.handleChange} />       
     </>
 }
