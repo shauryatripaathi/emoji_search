5ce6094b4e6f8e3de7ddcc63e5220e02513d95c4
Shaurya Tripathi <shauryatripaathi@gmail.com>
2022-05-20 17:36:15 +0530

    Update index.js

--- src/Body/EmojiGrid/index.js
+++ src/Body/EmojiGrid/index.js
@@ -1,17 +1,19 @@
 import './style.css'
 import data from '../../data/data.json'
-export default function EmojiGrid(props){
-    return(
+export default function EmojiGrid(props) {
+    const onClick = (e) => {
+        console.log('You Copied' + e.target.id)
+    }
+    return (
         <>
-        
-            {data.filter((emoji => emoji.keywords.includes(props.searchText))).map((item,index)=>{
+            {data.filter((emoji => emoji.keywords.includes(props.searchText))).map((item, index) => {
                 return (
-                    <div className='grid-Emoji' key={index}>
+                    <div onClick={onClick} className='grid-Emoji' id={item.symbol} key={index}>
                         {item.symbol}
                     </div>
                 )
             })}
-        
+
         </>
     )
 }
