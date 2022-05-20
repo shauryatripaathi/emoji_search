5d86c4d437f7ee9eb21045ceb6d5247cc5adc326
Shaurya Tripathi <shauryatripaathi@gmail.com>
2022-05-20 16:57:10 +0530

    update : responsive website

--- src/Header/SearchBar/index.js
+++ src/Header/SearchBar/index.js
@@ -1,6 +1,9 @@
+import './style.css'
+
 export default function SearchBar(props) {
     return <>
             <input type='search'
+                className="searchbar"
                 value={props.value}
                 onChange={props.handleChange} />       
     </>
